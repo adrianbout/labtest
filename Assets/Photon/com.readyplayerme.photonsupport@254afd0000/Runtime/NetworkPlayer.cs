98dcd6b6084ea65df921acefb099184ef41a2a06
adrian boutros <adrian.boutros.003@gmail.com>
2023-07-28 16:31:50 +0300

    Ready player me photon support

---
+++ Assets/Photon/com.readyplayerme.photonsupport@254afd0000/Runtime/NetworkPlayer.cs
@@ -0,0 +1,79 @@
+using Photon.Pun;
+using UnityEngine;
+using ReadyPlayerMe.AvatarLoader;
+
+/// <summary>
+///     Used on Ready Player Me 
+/// </summary>
+[RequireComponent(typeof(PhotonView))]
+public class NetworkPlayer : MonoBehaviour
+{
+    [SerializeField] private AvatarConfig config;
+
+    private Animator animator;
+    private PhotonView photonView;
+
+    private Transform leftEye;
+    private Transform rightEye;
+
+    private SkinnedMeshRenderer[] skinnedMeshRenderers;
+
+    private const string FULL_BODY_LEFT_EYE_BONE_NAME = "Armature/Hips/Spine/Spine1/Spine2/Neck/Head/LeftEye";
+    private const string FULL_BODY_RIGHT_EYE_BONE_NAME = "Armature/Hips/Spine/Spine1/Spine2/Neck/Head/RightEye";
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        photonView = GetComponent<PhotonView>();
+
+        leftEye = transform.Find(FULL_BODY_LEFT_EYE_BONE_NAME);
+        rightEye = transform.Find(FULL_BODY_RIGHT_EYE_BONE_NAME);
+
+        skinnedMeshRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
+    }
+
+    /// <summary>
+    ///     Calls PunRPC with the avatar URL as paramater to load the local and remote avatars.
+    /// </summary>
+    /// <param name="url">Avatar URL</param>
+    public void LoadAvatar(string url)
+    {
+        photonView.RPC("SetPlayer", RpcTarget.AllBuffered, url);
+    }
+
+    [PunRPC]
+    private void SetPlayer(string incomingUrl)
+    {
+        AvatarObjectLoader loader = new AvatarObjectLoader();
+        loader.LoadAvatar(incomingUrl);
+        loader.AvatarConfig = config;
+        loader.OnCompleted += (sender, args) =>
+        {
+            leftEye.transform.localPosition = args.Avatar.transform.Find(FULL_BODY_LEFT_EYE_BONE_NAME).localPosition;
+            rightEye.transform.localPosition = args.Avatar.transform.Find(FULL_BODY_RIGHT_EYE_BONE_NAME).localPosition;
+
+            TransferMesh(args.Avatar);
+        };
+    }
+
+    //TODO: Multiple mesh transfer support.
+    private void TransferMesh(GameObject source)
+    {
+        Animator sourceAnimator = source.GetComponentInChildren<Animator>();
+        SkinnedMeshRenderer[] sourceMeshes = source.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+        for (int i = 0; i < sourceMeshes.Length; i++)
+        {
+            Mesh mesh = sourceMeshes[i].sharedMesh;
+            skinnedMeshRenderers[i].sharedMesh = mesh;
+
+            Material[] materials = sourceMeshes[i].sharedMaterials;
+            skinnedMeshRenderers[i].sharedMaterials = materials;
+        }
+
+        Avatar avatar = sourceAnimator.avatar;
+        animator.avatar = avatar;
+
+        Destroy(source);
+    }
+}
